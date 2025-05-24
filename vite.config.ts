b3a7f9fbe6207fe9ea6e6d7e1e16d738909a6402
sattheone <sattheone@gmail.com>
2025-05-24 15:44:02 +0530

    Update build configuration for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -12,5 +12,13 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, '.'),
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
   }
 });
